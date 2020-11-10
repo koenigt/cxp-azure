48dc88216c6d488ae01e96a50ecb3968183eeea3
Thimo Koenig <thimo.koenig@msg-systems.com>
2020-11-10 17:32:40 +0100

    reset vnet to use default DNS servers

--- main.tf
+++ main.tf
@@ -90,7 +90,6 @@ resource "azurerm_virtual_network" "vnet-westeu-cn" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg-westeu-cn.name
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
 
 ############################
